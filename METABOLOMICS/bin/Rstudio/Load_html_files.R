3285ed984fd4aaab763a2daa7e42c24c8222d0d4
Vero <veronica.rg.pb@gmail.com>
2019-09-26 13:04:20 -0500

    add possibles packages

--- METABOLOMICS/bin/Rstudio/Load_html_files.R
+++ METABOLOMICS/bin/Rstudio/Load_html_files.R
@@ -5,7 +5,10 @@ rawHTML <- paste(readLines("../../metadata/GC-MS/LibSrch_10.htm"), collapse="\n"
 # second option xml2 and rvest
 
 library(xm12)
+
 library(rvest)
 html_dat <- read_html(
   system.file("../../metadata/GC-MS/LibSrch_10.htm", package = "textreadr")
 )
+
+html_table(x= "../../metadata/GC-MS/LibSrch_10.htm", package = "textreadr", header = NA, trim = TRUE, fill = FALSE, dec = ".")
