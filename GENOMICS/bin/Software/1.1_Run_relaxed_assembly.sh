7d12b5b35a095df7bb88747bd68881e10d6db8a9
Veronica Reyes <VeroIarrachtai@users.noreply.github.com>
2019-11-05 21:07:42 -0600

    add files

--- GENOMICS/bin/Software/1.1_Run_relaxed_assembly.sh
+++ GENOMICS/bin/Software/1.1_Run_relaxed_assembly.sh
@@ -1,6 +1,10 @@
-!#
-#
-#
+#! /bin/sh
 
+#SBATCH -p keri
+#SBATCH --mem 80000
+#SBATCH -n 2
 
-vcftools --vcf TMVB_5SNPradlocus.vcf --keep 89_ind.txt --max-missing 0.9 --maf 0.05 --recode --out 89ind_maxmiss0.9_maf0.05
+export LD_LIBRARY_PATH=/opt/miniconda2/lib
+export HDF5_USE_FILE_LOCKING=FALSE
+
+ipyrad -p .vcf -s 1234567 -f
